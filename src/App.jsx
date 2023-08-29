0f84e0c74dc1df7d8dfe205f07568ad003514a19
Tender <tender121995@gmail.com>
2023-08-29 19:47:57 +0300

    added-update-using-props-done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Home from "./components/Home/Home"
 import { getAllCars } from "./services/cars-service";
 import Add from "./components/Add/Add"
 import Update from "./components/Update/Update"
+import Card from "./components/Home/Card";
+import { Carousel } from "react-bootstrap";
 
 function App() {
 
   let [cars, setCars] = useState([]);
+
+  let [id, setId] = useState('');
 
   let [page, setPage] = useState("home")
 
@@ -25,14 +29,15 @@ function App() {
   return (
     <>
       {
-        page == "home" && <Home cars={cars} page={setPage} />
+        page == "home" && <Home cars={cars} page={setPage} setId={setId} />
       }
       {
         page == "new-car" && <Add page={setPage} />
       }
       {
-        page == "update-car" && <Update page={setPage} />
+        page == "update-car" && <Update page={setPage} id={id} setCars={setCars}/>
       }
+
     </>
   )
 }
